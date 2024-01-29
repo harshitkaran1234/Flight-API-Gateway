3aabc5cf96b6e934c73fc1953c7a75d6c38113b4
karanharshit <harsheet2020@gmail.com>
2024-01-29 08:53:45 +0530

    added rate limiter and reverse proxy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+const serverConfig = require('./config/server-config');
 
 const app = express();
 
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, //2minutes
+	limit: 3 // 3 req maxm
+})
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(limiter);
+
+app.use('/flightsService', createProxyMiddleware({ 
+    target: serverConfig.FLIGHT_SERVICE, 
+    changeOrigin: true,
+    pathRewrite: {'^/flightsService': '/'} 
+}));
+app.use('/bookingService', createProxyMiddleware({ 
+    target: serverConfig.BOOKING_SERVICE, 
+    changeOrigin: true,
+    pathRewrite: {'^/bookingService': '/'} 
+}));
 
 app.use('/api', apiRoutes);
 
